2ba21adfbf99f6170c42e5967b563dcd135f106f
Uthappa <gayanuthappams@gmail.com>
2024-10-30 18:37:09 -0400

    Remove docs and results files from tracking

--- docs/html/search/variables_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['classlist_5fpath_56',['CLASSLIST_PATH',['../app_2main_8cpp.html#a0e6e3ea0a4ca7ecb2c8fe6bdeb45c8b0',1,'CLASSLIST_PATH():&#160;main.cpp'],['../test_8cpp.html#a0e6e3ea0a4ca7ecb2c8fe6bdeb45c8b0',1,'CLASSLIST_PATH():&#160;test.cpp']]],
-  ['confidence_5fthreshold_57',['confidence_threshold',['../classDetector.html#a7ba1f5f4b0fcf47c937348c4db9e495c',1,'Detector']]],
-  ['confidence_5fthreshold_58',['CONFIDENCE_THRESHOLD',['../app_2main_8cpp.html#a5b908ab9fc958ed44b8fc0f9ca88cd49',1,'CONFIDENCE_THRESHOLD():&#160;main.cpp'],['../test_8cpp.html#a5b908ab9fc958ed44b8fc0f9ca88cd49',1,'CONFIDENCE_THRESHOLD():&#160;test.cpp']]]
-];
--- docs/html/search/variables_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pixels_64',['pixels',['../structDetectorOutput.html#abb16bc974e16274e879d010c023bf42e',1,'DetectorOutput']]]
-];
